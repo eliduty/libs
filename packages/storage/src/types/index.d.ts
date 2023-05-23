3ccff0915f11ca419ab1cf6183cb4c9b701c27fe
eliduty <eliduty@qq.com>
2023-05-23 16:32:49 +0800

    chore: 修改构建工具

--- packages/storage/src/types/index.d.ts
+++
@@ -1,8 +0,0 @@
-/*
- * @Author: eliduty
- * @Github: https://github.com/eliduty
- * @Date: 2022-01-28 17:30:24
- * @LastEditors: eliduty
- * @LastEditTime: 2022-02-11 18:44:48
- * @Description: 自定义类型声明文件
- */
